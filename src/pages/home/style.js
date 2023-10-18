8c48bd1d091126ea4a3accf82b065f37d73f4034
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-10-18 14:28:08 -0300

    css

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -2,74 +2,67 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     padding: 2rem;
-    background-color: #141414; /* Cor de fundo semelhante à da HBO Max */
+    background-color: #f0f0f0; 
+    font-family: Arial, sans-serif;
+    text-align: center;
 
     h1 {
-        text-align: center;
-        margin: 4rem 0;
-        color: #ffffff; /* Cor do texto branca */
-        font-size: 2rem;
+        font-size: 2.5rem;
+        margin: 2rem 0;
+        color: #333; 
     }
 `;
 
 export const MovieList = styled.ul`
     list-style: none;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-    column-gap: 3rem;
-    row-gap: 4rem;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    gap: 20px;
+    margin: 0;
+    padding: 0;
 `;
 
 export const Movie = styled.li`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    background-color: #282828; /* Cor de fundo semelhante à da HBO Max */
-    padding: 1rem;
+    background-color: #fff;
     border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    transition: transform 0.2s;
 
     img {
-        width: 180px;
-        border-radius: 8px;
-        margin-bottom: 1rem;
+        width: 100%;
+        border-radius: 8px 8px 0 0;
+    }
+
+    &:hover {
+        transform: translateY(-5px);
     }
 
     span {
         font-weight: bold;
         font-size: 1.2rem;
-        text-align: center;
-        color: #ffffff; /* Cor do texto branca */
+        padding: 1rem;
+        color: #333;
     }
 
     a {
-        transition: all 0.3s;
         text-decoration: none;
-        color: #ffffff; /* Cor do texto branca */
-    }
-
-    a:hover {
-        transform: scale(1.1);
     }
 `;
 
 export const Btn = styled.button`
-    margin-top: 5px;
-    padding: 0.7rem 3rem;
+    margin-top: 10px;
+    padding: 0.5rem 2rem;
     border: none;
-    border-radius: 15px;
-    color: #ffffff;
-    background-color: #ff0000; /* Cor semelhante à da HBO Max */
+    border-radius: 4px;
+    color: #fff;
+    background-color: #ff5722; 
     font-weight: bold;
-    font-size: 0.9rem;
+    font-size: 1rem;
     cursor: pointer;
-    transition: all 250ms;
-    text-transform: uppercase;
-    letter-spacing: 1px;
+    transition: background-color 0.2s;
 
     &:hover {
-        background-color: #ff2222; /* Cor mais clara ao passar o mouse */
+        background-color: #ff4500; 
     }
 `;
 
